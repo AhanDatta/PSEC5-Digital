5cbb930d928f195b017485f4bff54ebdb15a9b96
AhanDatta <ahan.datta312@gmail.com>
2024-05-10 13:55:14 -0500

    Update PICO.sv
    Need to fix this - I believe there is something wrong with the "latched_msg" in module read_write.

--- PICO.sv
+++ PICO.sv
@@ -84,15 +84,15 @@ input logic serial_in,
 input logic spi_clk,
 input logic iclk, //internal clock
 output logic [7:0] full_msg,
-output logic msg_stop_rstn // reset not from clock comparator -> register
+output logic msg_donen, // active-low from buffer reg saying when msg is complete
+output logic sclk_stop_rstn // reset not from clock comparator -> register
 );
 
-    logic [7:0] msgi;
-    logic rstin; //internal reset not from reg -> shift reg 
+    logic [7:0] msgi; //Internal msg from shift register to buffer reg
 
-    s2p_shift_register shift_reg(.serial_in (serial_in), .sclk (spi_clk), .rstn (rstin), .msg (msgi));
-    buffer_register register(.msg (msgi), .sclk (spi_clk), .full_rstn (msg_stop_rstn), .rstn (rstin), .complete_msg (full_msg));
-    clock_comparator comparator(.sclk (spi_clk), .iclk (iclk), .full_rstn (msg_stop_rstn));
+    s2p_shift_register shift_reg(.serial_in (serial_in), .sclk (spi_clk), .rstn (sclk_stop_rstn), .msg (msgi));
+    buffer_register register(.msg (msgi), .sclk (spi_clk), .full_rstn (sclk_stop_rstn), .rstn (msg_donen), .complete_msg (full_msg));
+    clock_comparator comparator(.sclk (spi_clk), .iclk (iclk), .full_rstn (sclk_stop_rstn));
 endmodule
 
 // 8bit -> logic (first 8bit -> pointer for reg number, next 8 -> data, increment register, repeat until msg stop)
@@ -138,17 +138,59 @@ endmodule
 //Also latches signal from previous block
 module read_write (
 input logic [7:0] msg, //message from the s2p_module
+input logic latch_en, //Comes from the buffer register
 input logic rstn, //from the clock comparator to clear the address pointer
 input logic sclk, //Spi clock 
+output logic [7:0] write_data, //the data that is 
 output logic [7:0] mux_control_signal //controls the mux which regulates reads out the chosen digital reg
 );
     //"latched_msg" is the last non-zero value of msg
     //Name is a little misleading but forgive me
     logic [7:0] latched_msg;
-    logic latch_en = (msg != '0);
     synch_reg msg_latcher (.d (msg), .en (latch_en), .sclk (sclk), .rstn (rstn), .q (latched_msg));
 
     //Logic for addressing registers and writing
-    //UNFINISHED
     logic [7:0] address_pointer = '0;
+    always_ff @(posedge sclk or negedge rstn) begin
+        if (!rstn) begin
+            address_pointer <= '0;
+            write_data <= '0;
+        end
+        else begin
+            if (address_pointer == '0) begin
+                address_pointer <= latched_msg;
+                write_data <= '0;
+            end
+            else begin
+                address_pointer <= address_pointer + 1;
+                write_data <= latched_msg;
+            end
+        end
+    end
+
+    //Outputs the address pointer into the mux_control
+    always_comb begin
+        if (!rstn) begin
+            mux_control_signal = '0;
+        end
+        else begin
+            mux_control_signal <= address_pointer;
+        end
+    end
+endmodule
+
+module PICO (
+input logic serial_in, //Serial message data
+input logic sclk, //spi clock
+input logic iclk, //internal clock
+output logic [7:0] write_data, //Output data to write
+output logic [7:0] mux_control_signal //Output control signal for POCI mux
+); 
+    logic [7:0] msgi; //internal message from s2p -> read_write
+    logic msg_donen; //active-low msg done signal from buffer register
+    logic sclk_stop_rstn; //From the clock comparator
+
+    s2p_module serial_to_eight_bit (.serial_in (serial_in), .spi_clk (sclk), .iclk (iclk), .full_msg (msgi), .msg_donen (msg_donen), .sclk_stop_rstn (sclk_stop_rstn));
+    read_write eight_bit_to_output (.msg (msgi), .latch_en (!msg_donen), .rstn (sclk_stop_rstn), .sclk (sclk), .write_data (write_data), .mux_control_signal (mux_control_signal));
+
 endmodule
