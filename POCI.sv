9a05ecc0ccd4503fb8aed23bd105d3ca06f2817c
xpawesome <arzacandrew@gmail.com>
2024-05-22 13:05:13 -0500

    Update POCI.sv
    This code isn't working right now, for some reason, it has an unexpected token of 'endmodule' which I don't understand, I'm currently in finals so I will make changes and build out this entire section in roughly a week once my tests are finished

---
+++ POCI.sv
@@ -0,0 +1,20 @@
+//8 bit message into serial
+
+module address (input logic [7:0] addr, input logic sclk, input logic rstn, output logic serial_out);
+
+// addr is the 7 bit input, being turned serial
+// rstn is the reset
+
+logic [7:0]q;
+
+always_ff @(posedge sclk or negedge rstn) begin
+	if (!rstn) begin
+ q<= 8'b00000000;
+ end
+ 	else begin
+serial_out <= q[0];
+q <= q >> 1;
+end
+
+
+endmodule
