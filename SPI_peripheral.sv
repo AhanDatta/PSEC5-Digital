65bc7001b6ed04db8e5e4e0f1d1d8fb82ecc4388
AhanDatta <ahan.datta312@gmail.com>
2024-06-05 10:04:30 -0500

    Setting up full SPI Peripheral Block

--- SPI_peripheral.sv
+++ SPI_peripheral.sv
@@ -9,4 +9,20 @@ module SPI (
     output logic serial_out
 );
 
+    logic sclk_stop_rstn;
+    logic [7:0] write_data;
+    logic [7:0] mux_control_signal;
+    wire full_rstn = rstn && sclk_stop_rstn;
+
+    PICO input (
+        .serial_in (serial_in), 
+        .sclk (sclk), 
+        .iclk (iclk), 
+        .rstn (rstn), 
+        .sclk_stop_rstn (sclk_stop_rstn), 
+        .write_data (write_data), 
+        .mux_control_signal (mux_control_signal)
+    );
+
+
 endmodule
