20bd98077b0e2dd46f3401e9519b60abad3f6277
AhanDatta <ahan.datta312@gmail.com>
2024-06-06 15:26:15 -0500

    testbench changes

--- testbench.sv
+++ testbench.sv
@@ -49,3 +49,27 @@ class generator;
 
 endclass
 
+interface intf (
+    input logic sclk, //spi clock
+    input logic iclk, //internal clock
+    input logic rstn, //external reset
+);
+
+    //Simulates the data sitting in the analog registers
+    rand logic [49:0] ch0;
+    rand logic [49:0] ch1;
+    rand logic [49:0] ch2;
+    rand logic [49:0] ch3;
+    rand logic [49:0] ch4;
+    rand logic [49:0] ch5;
+    rand logic [49:0] ch6;
+    rand logic [49:0] ch7;
+
+    //Holds a variable length array of input messages
+    //Gets converted to serial by the driver
+    logic [7:0] input_bytes [];
+    //From the output of the DUT
+    //Length should be the same as "input_bytes"
+    logic [7:0] output_bytes [];
+    
+endinterface
