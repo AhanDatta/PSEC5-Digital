863638c94726890adeacd848d0ff0bb0d8fff194
xpawesome <arzacandrew@gmail.com>
2024-07-10 14:15:28 -0500

    Update transaction.sv

--- tb_stuff/transaction.sv
+++ tb_stuff/transaction.sv
@@ -9,7 +9,7 @@ class transaction;
   rand logic [49:0] ch4;
   rand logic [49:0] ch5;
   rand logic [49:0] ch6;
-  rand logic [49:0] ch7;
+ // rand logic [49:0] ch7;
   rand logic serial_in;
   logic serial_out;
 
@@ -17,7 +17,7 @@ class transaction;
     $display ("----------------");
     $display ("tag= %s", tag);
     $display ("----------------");
-    $display ("ch 0 = %0d, ch 1 = %0d, ch 2 = %0d, ch 3 = %0d, ch 4 = %0d, ch 5 = %0d, ch 6 = %0d, ch 7 = %0d, ", ch0, ch1, ch2, ch3, ch4, ch5, ch6, ch7);
+    $display ("ch 0 = %0d, ch 1 = %0d, ch 2 = %0d, ch 3 = %0d, ch 4 = %0d, ch 5 = %0d, ch 6 = %0d, ch 7 = %0d, ", ch0, ch1, ch2, ch3, ch4, ch5, ch6); //,ch7);
     $display ("serial_in = %0d, serial_out = %0d", serial_in, serial_out);
 
   endfunction
