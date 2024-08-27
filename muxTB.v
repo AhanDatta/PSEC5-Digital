c428f1d4c195ae0df4967b4322940c97b44d6996
xpawesome <arzacandrew@gmail.com>
2024-08-27 10:14:28 -0500

    Update muxTB.v

--- muxTB.v
+++ muxTB.v
@@ -23,6 +23,10 @@
 //Testbench to verify SPI block's new design with MUX included
 module SpiMuxTB();
 
+parameter MSG_SIZE = 1; //Determins how many bytes serial in will be
+
+
+
 //inputs into SPI block
 reg serial_in;
 reg sclk;
@@ -95,6 +99,9 @@ wire serial_out_mux;
 
 
 integer counter;
+integer serial_in_counter;
+reg onoff_serial_in;
+reg[MSG_SIZE*8:0] total_msg;
 
 
 
@@ -484,15 +491,153 @@ always begin
     #25 sclk = ~sclk;
 end
 
+always begin
+    #25 iclk = ~iclk;
+end
+
 // F Clock (0.2ns)
 always begin
     #0.2 FCLK = ~FCLK;
 end
 
+
+//Counter to keep track of total time passed in simulation
 always begin
     #1 counter <= counter + 1;
 end
 
 
+always @(posedge sclk) begin
+
+    if (onoff_serial_in > 0) begin
+            if (serial_in_counter < 8*MSG_SIZE) begin
+    
+                serial_in = total_msg[serial_in_counter];
+                serial_in_counter <= serial_in_counter + 1;
+            end
+            
+            else begin
+            
+            onoff_serial_in <= 1'b0;
+            #5
+            serial_in_counter = 0;
+            end
+            
+    end
+
+end
+
+
+        always begin // Timed delay of DISCRIMINATOR_OUTPUT, after 25ns it flips every 2ns from there on
+        #5
+            if (counter > 55) begin
+              DISCRIMINATOR_OUTPUT = ~DISCRIMINATOR_OUTPUT;
+              end
+            end
+
+
+
+
+initial begin
+
+counter <= 0;
+serial_in_counter <= 0;
+onoff_serial_in <= 1'b0;
+
+
+//Initializing Clocks
+sclk <= 1'b0;
+iclk <= 1'b0;
+FCLK <= 1'b0;
+
+#25
+
+//Global Reset
+rstn <= 1'b1;
+RSTB <= 1'b1;
+#5
+rstn <= 1'b0;
+RSTB <= 1'b0;
+#10
+rstn <= 1'b1;
+RSTB <= 1'b1;
+#5
+
+
+
+
+//Initializing Channel Values, CA-CE
+
+CA0 <= 10'b0000000000;
+CB0 <= 10'b0000000001;
+CC0 <= 10'b0000000010;
+CD0 <= 10'b0000000100;
+CE0 <= 10'b0000001000;
+
+CA1 <= 10'b0000010000;
+CB1 <= 10'b0000100000;
+CC1 <= 10'b0001000000;
+CD1 <= 10'b0010000000;
+CE1 <= 10'b0100000000;
+
+CA2 <= 10'b1111111111;
+CB2 <= 10'b1111100000;
+CC2 <= 10'b0000011111;
+CD2 <= 10'b1111000011;
+CE2 <= 10'b1010101010;
+
+CA3 <= 10'b1010100101;
+CB3 <= 10'b1001010101;
+CC3 <= 10'b0010011011;
+CD3 <= 10'b0100001111;
+CE3 <= 10'b0100100111;
+
+CA4 <= 10'b0011101011;
+CB4 <= 10'b0101010101;
+CC4 <= 10'b0011010111;
+CD4 <= 10'b1111011101;
+CE4 <= 10'b1111101110;
+
+CA5 <= 10'b0101111010;
+CB5 <= 10'b0000010001;
+CC5 <= 10'b0101000100;
+CD5 <= 10'b0010011010;
+CE5 <= 10'b0101000010;
+
+CA6 <= 10'b0010010010;
+CB6 <= 10'b1001010001;
+CC6 <= 10'b0100110010;
+CD6 <= 10'b0101001101;
+CE6 <= 10'b1000101010;
+
+CA7 <= 10'b1001001001;
+CB7 <= 10'b1001010101;
+CC7 <= 10'b0101010011;
+CD7 <= 10'b1001010101;
+CE7 <= 10'b0101010100;
+
+//Remaining Input Initialization for SPI_Peripheral
+pll_locked <= 8'b00000000;
+total_msg <= 8'b01010000;
+
+//Initializing Remaining Inptus for psec5_digital_august2024
+INST_STOP <= 1'b0; //Initializing values of variables in the block
+INST_READOUT <= 1'b0; 
+//MODE <= 2'b00;
+TRIG_DELAY <= 4'b0010;
+DISCRIMINATOR_OUTPUT <= 1'b0;
+SELECT_REG <= 3'b000;
+
+
+//Initializing Inputs for serial_out_mux
+raw_serial_out <= 8'b00000000;
+wr_serial_out <= 1'b0;
+
+#10
+
+//Inputting serial_in values
+onoff_serial_in <= 1'b1;
+
+end
 
 endmodule
